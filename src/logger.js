3d66eaa8ecedfdd125e443b88509d6523a31f862
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2023-02-16 21:39:34 -0300

    feat(logger): add a console transport and a debug file

--- src/logger.js
+++ src/logger.js
@@ -9,6 +9,8 @@ const logger = winston.createLogger({
     transports: [
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
         new winston.transports.File({ filename: 'info.log', level: 'info' }),
+        new winston.transports.File({ filename: 'debug.log', level: 'debug' }),
+        new winston.transports.Console({level: 'silly'})
     ],
 });
  
