39e67be3706f9a3b92e12d36a52d5f5d16d86bf2
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2023-02-16 21:13:04 -0300

    feat(dependencies): install and set up winston

--- src/printMyName.js
+++ src/printMyName.js
@@ -1,19 +1,27 @@
+const logger = require('../logger')
+
 /**
  * Concatenate names from a vector of names
  */
 function getFullName(names) {
     let fullName = '';
     names.forEach(name => {
-        fullName += name + ' ';
+        fullName += name + 'X';
     });
     return fullName;
 }
 
 function printFullName(fullname) {
-    console.log(fullname)
+    console.log(fullname.replaceAll('X', ' '))
+    logger.debug('logging debug')
+    logger.info('logging info')
+    logger.warn('logging warn')
+    logger.error('logging error')
 }
 
 module.exports = {
     getFullName,
     printFullName
 }
+
+printFullName("alcides augusto bezerra neto")
