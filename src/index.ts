e5b7d8ccbbbaa773f9e45f80d109d1ffaf991162
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2023-03-21 00:19:59 -0300

    feat: set up the highest size of logs
    style code
    implent a more sofisticated example

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,51 @@
-import winston, { Logger, createLogger, format, transports } from 'winston';
+import winston, { Logger, createLogger, format, transports } from "winston";
 
-
-
-export const logsPath: string = "logs/"
-
+export const logsPath: string = "logs/";
 
 /**
- * Logger is created with the famous logger winston, 
+ * Logger is created with the famous logger winston,
  * so use .info('...') or .debug('...') or .error(...) and so on.
- * 
+ *
  * logger.error('qwerty')
  */
 export const logger: Logger = createLogger({
-    format: format.combine(
-        format.timestamp(),
-        format.printf(info => {
-            return `${info.timestamp} [${info.level.toUpperCase()}] - ${info.message}`;
-        })
-    ),
-    transports: [
-        new transports.File({ filename: `${logsPath}error.log`, level: 'error', format: format.json() }),
-        new transports.File({ filename: `${logsPath}info.log`, level: 'info', format: format.json() }),
-        new transports.File({ filename: `${logsPath}debug.log`, level: 'debug', format: format.json() }),
-        new transports.Console({level: 'warn'})
-    ],
+  format: format.combine(
+    format.timestamp(),
+    format.printf((info) => {
+      return `${info.timestamp} [${info.level.toUpperCase()}] - ${
+        info.message
+      }`;
+    })
+  ),
+  transports: [
+    new transports.File({
+      level: "error",
+      filename: `${logsPath}error_${new Date().toISOString()}.log`,
+      format: format.json(),
+      maxsize: 4096,
+    }),
+    new transports.File({
+      level: "debug",
+      filename: `${logsPath}debug_${new Date().toISOString()}.log`,
+      format: format.json(),
+      maxsize: 4096,
+      maxFiles: 2,
+    }),
+    new transports.Console({ level: "warn" }),
+  ],
 });
- 
+
 if (process.env.ADS_MYLOG_CONSOLE_LEVEL) {
-    logger.add(new winston.transports.Console({
-        level: String(process.env.ADS_MYLOG_CONSOLE_LEVEL)
-    }));
+  logger.add(
+    new winston.transports.Console({
+      level: String(process.env.ADS_MYLOG_CONSOLE_LEVEL),
+    })
+  );
 }
 
 export default logger;
-export const log: Logger = logger
+
+/**
+ * Shorter than logger (same feature).
+ */
+export const log: Logger = logger;
--- src/simpleClientExample.ts
+++ src/simpleClientExample.ts
@@ -1,4 +1,13 @@
 import logger from ".";
 
-logger.error("errrrrroooooououo");
-logger.debug("debuuuuuug");
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+(async function () {
+  for (let i = 0; i < 100; i++) {
+    logger.error("errrrrroooooououo");
+    await sleep(500);
+    logger.debug("de d debuuuuuug");
+  }
+})();
